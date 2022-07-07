a5a0b6bf5823243269042052e58c83005bbdaf89
kurziokode <108660505+kurziokode@users.noreply.github.com>
2022-07-07 07:50:31 +0300

    Update main.js

--- hack/main.js
+++ hack/main.js
@@ -42,14 +42,7 @@
 
 
     // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        items: 1,
-        dots: true,
-        loop: true,
-        nav : false
-    });
+
 
 
     // Chart Global Color
