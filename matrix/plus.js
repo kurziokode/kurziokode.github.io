d21a708a68a510705bd626365c061ce28001b370
kurziokode <108660505+kurziokode@users.noreply.github.com>
2022-09-04 15:39:46 +0300

    Update plus.js

--- matrix/plus.js
+++ matrix/plus.js
@@ -341,6 +341,30 @@ for (let i = 0; i < lis.length; i++) {
 
 
 
+   document.onclick = hideMenu;
+        document.oncontextmenu = rightClick;
+  
+        function hideMenu() {
+            document.getElementById(
+                "contextMenu").style.display = "none"
+        }
+  
+        function rightClick(e) {
+            e.preventDefault();
+  
+            if (document.getElementById(
+                "contextMenu").style.display == "block")
+                hideMenu();
+            else {
+                var menu = document
+                    .getElementById("contextMenu")
+                      
+                menu.style.display = 'block';
+                menu.style.left = e.pageX + "px";
+                menu.style.top = e.pageY + "px";
+            }
+        }
+
 
 
 
