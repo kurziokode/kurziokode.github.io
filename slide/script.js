dcbe379344a8ad58b8c8dfa8099b826a1b7ffbc8
kurziokode <108660505+kurziokode@users.noreply.github.com>
2022-09-13 06:07:14 +0300

    Create script.js

---
+++ slide/script.js
@@ -0,0 +1,17 @@
+$('body').vegas({
+  overlay: true,
+  transition: 'fade', 
+  transitionDuration: 4000,
+  delay: 10000,
+  color: 'red',
+  animation: 'random',
+  animationDuration: 20000,
+  slides: [
+    { src: 'https://jacksmith206.github.io/teetotaller/moto.jpg' },
+    { src: 'https://jacksmith206.github.io/teetotaller/lee.jpg' },
+    { src: 'https://jacksmith206.github.io/teetotaller/about.jpg' },
+    { src: 'https://jacksmith206.github.io/teetotaller/1.png' },
+    { src: 'https://kurziokode.github.io/hack/curzio.jpg' }
+  
+  ]
+});
