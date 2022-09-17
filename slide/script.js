84c763e3d65938eaea39ff5dd38ee64a9e873145
kurziokode <108660505+kurziokode@users.noreply.github.com>
2022-09-17 06:24:35 +0300

    Update script.js

--- slide/script.js
+++ slide/script.js
@@ -11,6 +11,7 @@ $('body').vegas({
     { src: 'https://jacksmith206.github.io/teetotaller/lee.jpg' },
     { src: 'https://jacksmith206.github.io/teetotaller/about.jpg' },
     { src: 'https://jacksmith206.github.io/teetotaller/1.png' },
+    { src: 'https://jacksmith206.github.io/red/img/user.png' },
     { src: 'https://kurziokode.github.io/hack/curzio.jpg' }
   
   ]
