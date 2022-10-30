e027702d194c3a4a61f1a86a90f6367a70c7eeb8
kurziokode <108660505+kurziokode@users.noreply.github.com>
2022-10-30 17:52:54 +0200

    Update trash.js

--- slide/trash.js
+++ slide/trash.js
@@ -8,6 +8,7 @@ $('body').vegas({
   animationDuration: 8000,
   slides: [
     { src: 'https://www.athensmagazine.gr/photos/w_800px/articles/201812/377f97_2.jpg' },
+    { src: 'https://i1.prth.gr/images/963x541/files/2019-12-17/bo12.jpg' },
     { src: 'https://aegeanews.gr/wp-content/uploads/2017/07/anna-panagiotarea-errikos-ntynan.jpg' },
     { src: 'https://www.ilovestyle.com/sites/default/files/454545454555555.jpg' },
     { src: 'https://www.in.gr/wp-content/uploads/2022/07/adonis-manolidoy.jpg' },
