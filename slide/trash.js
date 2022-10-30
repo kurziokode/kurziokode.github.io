1f8c5f8b5168f5c7dd6d38ac1fe82e135b34c732
kurziokode <108660505+kurziokode@users.noreply.github.com>
2022-10-30 16:30:01 +0200

    Update trash.js

--- slide/trash.js
+++ slide/trash.js
@@ -8,18 +8,12 @@ $('body').vegas({
   animationDuration: 8000,
   slides: [
     { src: 'https://www.athensmagazine.gr/photos/w_800px/articles/201812/377f97_2.jpg' },
- 
-   
     { src: 'https://aegeanews.gr/wp-content/uploads/2017/07/anna-panagiotarea-errikos-ntynan.jpg' },
-     { src: 'https://www.ilovestyle.com/sites/default/files/454545454555555.jpg' },
+    { src: 'https://www.ilovestyle.com/sites/default/files/454545454555555.jpg' },
     { src: 'https://www.in.gr/wp-content/uploads/2022/07/adonis-manolidoy.jpg' },
     { src: 'https://www.ekriti.gr/sites/default/files/2022-02/benizelos.jpg' },
-    { src: 'https://www.kontranews.gr/wp-content/uploads/2019/07/21393268.jpg' },
+    { src: 'https://www.kontranews.gr/wp-content/uploads/2019/07/21393268.jpg' }
  
- 
-     { src: '' }
- 
-   
-  
+
   ]
 });
