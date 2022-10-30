36839e5d5e57c42511b0b45969e070c7f9019956
kurziokode <108660505+kurziokode@users.noreply.github.com>
2022-10-30 14:57:08 +0200

    Create trash.js

---
+++ slide/trash.js
@@ -0,0 +1,25 @@
+$('body').vegas({
+  overlay: true,
+  transition: 'fade', 
+  transitionDuration: 3000,
+  delay: 8000,
+  color: 'red',
+  animation: 'random',
+  animationDuration: 15000,
+  slides: [
+    { src: '' },
+ 
+   
+    { src: '' },
+     { src: '' },
+    { src: '' },
+    { src: '' },
+    { src: '' },
+ 
+ 
+     { src: '' }
+ 
+   
+  
+  ]
+});
