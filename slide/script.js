c6976d17024dc3e6c22b1732c8089a85bafec6f4
kurziokode <108660505+kurziokode@users.noreply.github.com>
2023-09-10 11:43:08 +0300

    Update script.js

--- slide/script.js
+++ slide/script.js
@@ -2,24 +2,17 @@ $('body').vegas({
   overlay: true,
   transition: 'fade', 
   transitionDuration: 3000,
-  delay: 8000,
+  delay: 3000,
   color: 'red',
   animation: 'random',
-  animationDuration: 15000,
+  animationDuration: 30000,
   slides: [
     { src: 'https://kurziokode.github.io/1.jpg' },
- 
-   
     { src: 'https://kurziokode.github.io/7.jpg' },
-     { src: 'https://kurziokode.github.io/6.jpg' },
+    { src: 'https://kurziokode.github.io/6.jpg' },
     { src: 'https://kurziokode.github.io/5.jpg' },
     { src: 'https://kurziokode.github.io/4.png' },
     { src: 'https://kurziokode.github.io/3.jpg' },
- 
- 
-     { src: 'https://kurziokode.github.io/11.jpg' }
- 
-   
-  
+    { src: 'https://kurziokode.github.io/11.jpg' }
   ]
 });
