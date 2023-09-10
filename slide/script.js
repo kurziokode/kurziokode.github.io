bb92895ff812f4b4d0f75e8275214fa074bdf6c5
kurziokode <108660505+kurziokode@users.noreply.github.com>
2023-09-10 11:56:12 +0300

    Update script.js

--- slide/script.js
+++ slide/script.js
@@ -1,18 +1,18 @@
 $('body').vegas({
   overlay: true,
   transition: 'fade', 
-  transitionDuration: 3000,
-  delay: 3000,
+  transitionDuration: 10000,
+  delay: 10000,
   color: 'red',
   animation: 'random',
-  animationDuration: 30000,
+  animationDuration: 100000,
   slides: [
     { src: 'https://kurziokode.github.io/1.jpg', properties: { width: '100%', height: 'auto' }  },
-    { src: 'https://kurziokode.github.io/7.jpg' },
-    { src: 'https://kurziokode.github.io/6.jpg' },
-    { src: 'https://kurziokode.github.io/5.jpg' },
-    { src: 'https://kurziokode.github.io/4.png' },
-    { src: 'https://kurziokode.github.io/3.jpg' },
-    { src: 'https://kurziokode.github.io/11.jpg' }
+    { src: 'https://kurziokode.github.io/7.jpg', properties: { width: '100%', height: 'auto' }  },
+    { src: 'https://kurziokode.github.io/6.jpg', properties: { width: '100%', height: 'auto' } },
+    { src: 'https://kurziokode.github.io/5.jpg', properties: { width: '100%', height: 'auto' }  },
+    { src: 'https://kurziokode.github.io/4.png', properties: { width: '100%', height: 'auto' }  },
+    { src: 'https://kurziokode.github.io/3.jpg', properties: { width: '100%', height: 'auto' }  },
+    { src: 'https://kurziokode.github.io/11.jpg', properties: { width: '100%', height: 'auto' }  }
   ]
 });
