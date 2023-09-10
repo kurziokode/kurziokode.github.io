13353c5382cf90c63c2f6c26addbda8f28ecc635
kurziokode <108660505+kurziokode@users.noreply.github.com>
2023-09-10 13:38:23 +0300

    Update script.js

--- slide/script.js
+++ slide/script.js
@@ -9,7 +9,7 @@ $('body').vegas({
   slides: [
     { src: 'https://kurziokode.github.io/1.jpg', properties: { width: '100%', height: 'auto' }  },
     { src: 'https://kurziokode.github.io/7.jpg', properties: { width: '100%', height: 'auto' }  },
-    { src: 'https://kurziokode.github.io/6.jpg', properties: { width: '100%', height: 'auto' } },
+    { src: 'https://kurziokode.github.io/6.jpg', properties: { width: '100%', height: 'auto' }  },
     { src: 'https://kurziokode.github.io/5.jpg', properties: { width: '100%', height: 'auto' }  },
     { src: 'https://kurziokode.github.io/4.png', properties: { width: '100%', height: 'auto' }  },
     { src: 'https://kurziokode.github.io/3.jpg', properties: { width: '100%', height: 'auto' }  },
