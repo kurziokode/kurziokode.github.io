21c2e38e7cc73b00d313b9d9600bb3c2970908f3
kurziokode <108660505+kurziokode@users.noreply.github.com>
2023-09-10 14:39:31 +0300

    Update script.js

--- slide/script.js
+++ slide/script.js
@@ -8,12 +8,12 @@ $('body').vegas({
   animationDuration: 21000,
   slides: [
     { src: 'https://kurziokode.github.io/1.jpg', properties: { width: '100%', height: 'auto' }  },
-    { src: 'https://kurziokode.github.io/4242.jpg', properties: { width: '100%', height: 'auto' }  },
     { src: 'https://kurziokode.github.io/7.jpg', properties: { width: '100%', height: 'auto' }  },
     { src: 'https://kurziokode.github.io/6.jpg', properties: { width: '100%', height: 'auto' }  },
     { src: 'https://kurziokode.github.io/5.jpg', properties: { width: '100%', height: 'auto' }  },
-    { src: 'https://kurziokode.github.io/4243.jpg', properties: { width: '100%', height: 'auto' }  },
     { src: 'https://kurziokode.github.io/3.jpg', properties: { width: '100%', height: 'auto' }  },
+    { src: 'https://kurziokode.github.io/4243.jpg', properties: { width: '100%', height: 'auto' }  },
+    { src: 'https://kurziokode.github.io/4244.jpg', properties: { width: '100%', height: 'auto' }  },
     { src: 'https://kurziokode.github.io/11.jpg', properties: { width: '100%', height: 'auto' }  }
   ]
 });
