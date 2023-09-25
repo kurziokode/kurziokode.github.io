8514bfe90371c195de57aa983d129c70def69f80
kurziokode <108660505+kurziokode@users.noreply.github.com>
2023-09-25 23:22:28 +0300

    Update script.js

--- morph/script.js
+++ morph/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     // Function to start the marquee animation
     function startMarquee() {
-        $(".marquee").animate({ left: "-100%" }, 10000, "linear", function() {
+        $(".marquee").animate({ right: "100%" }, 10000, "linear", function() {
             // Animation complete, reset position and restart
-            $(this).css("left", "100%");
+            $(this).css("right", "-100%");
             startMarquee();
         });
     }
@@ -15,7 +15,8 @@ $(document).ready(function() {
     $(".marquee-container").hover(function() {
         $(".marquee").stop();
     }, function() {
-        $(".marquee").css("left", "100%");
+        $(".marquee").css("right", "-100%");
         startMarquee();
     });
 });
+
