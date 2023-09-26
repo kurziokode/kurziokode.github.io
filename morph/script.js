0ce8fb79eaaa63f27022538cfebc44bc99d51a7c
kurziokode <108660505+kurziokode@users.noreply.github.com>
2023-09-26 06:35:43 +0300

    Update script.js

--- morph/script.js
+++ morph/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   let animationInterval;
 
   function startMarquee() {
-    $(".marquee").animate({ left: "-100%" }, 15000, "linear", function() {
+    $(".marquee").animate({ left: "-100%" }, 60000, "linear", function() {
       $(this).css("left", "100%");
       if (!$(this).hasClass("paused")) {
         startMarquee();
