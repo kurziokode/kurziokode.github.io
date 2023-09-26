f4db8ff820f0a4d43033bb84bd9d9016a935dd60
kurziokode <108660505+kurziokode@users.noreply.github.com>
2023-09-26 05:50:57 +0300

    Update script.js

--- morph/script.js
+++ morph/script.js
@@ -1,66 +1,42 @@
-$(document).ready(function() {
-    let animationInterval;
+//$(document).ready(function() {
+   // let animationInterval;
     
     // Function to start the marquee animation
-    function startMarquee() {
-        $(".marquee").animate({ left: "-100%" }, 30000, "linear", function() {
+  //  function startMarquee() {
+    //    $(".marquee").animate({ left: "-100%" }, 30000, "linear", function() {
             // Animation complete, reset position and restart
-            $(this).css("left", "100%");
-            if (!$(this).hasClass("paused")) {
-                startMarquee();
-            }
-        });
-    }
+         //   $(this).css("left", "100%");
+          //  if (!$(this).hasClass("paused")) {
+           //     startMarquee();
+       //     }
+     //   });
+ //   }
 
     // Start the marquee animation on page load
-    startMarquee();
+  //  startMarquee();
 
-$('.marquee_text').marquee({
-  pauseOnHover: true
-});
+$(document).ready(function() {
+  let animationInterval;
 
-   $(".marquee-container").on('mouseenter', function() {
-    $(".marquee").stop().addClass("paused");
-}).on('mouseleave', function() {
-    $(".marquee").removeClass("paused");
-    startMarquee();
-});
-
-    <marquee onmouseover="this.stop();" onmouseout="this.start();">
-  my text here
-</marquee>
-
-$(function() {
-  var marquee = $('div.marquee');
-  marquee.each(function() {
-    var mar = $(this),
-        indent = mar.width();
-    mar.marquee = function() {
-      indent--;
-      mar.css('text-indent', indent);
-      if (indent < -1 * mar.children('div.marquee-text').width()) {
-        indent = mar.width();
-      }
-    };
-    mar.data('interval', setInterval(mar.marquee, 1000 / 60));
-    mar.hover(function() {
-      clearInterval($(this).data("interval"));
-    }, function() {
-      $(this).data('interval', setInterval(mar.marquee, 1000 / 60));
-    });
-  });
-});
-    
-    // Pause the marquee on hover
-    $(".marquee-container").hover(function() {
-        $(".marquee").stop().addClass("paused");
-    }, function() {
-        $(".marquee").removeClass("paused");
+  function startMarquee() {
+    $(".marquee").animate({ left: "-100%" }, 30000, "linear", function() {
+      $(this).css("left", "100%");
+      if (!$(this).hasClass("paused")) {
         startMarquee();
+      }
     });
+  }
+
+  $(".marquee-container").hover(
+    function() {
+      $(".marquee").stop(true).addClass("paused");
+    },
+    function() {
+      $(".marquee").removeClass("paused");
+      startMarquee();
+    }
+  );
+
+  // Start the marquee animation on page load
+  startMarquee();
 });
-
-
-
-
-
