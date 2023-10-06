56a9e0d3311e188a8423924721d1a2d306bfe461
kurziokode <108660505+kurziokode@users.noreply.github.com>
2023-10-06 14:26:15 +0300

    Update stream.js

--- morph/stream.js
+++ morph/stream.js
@@ -50,47 +50,52 @@ var tag = document.createElement('script');
   });
 */
 
-    var tag = document.createElement('script');
-    tag.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-    var player;
-    function onYouTubeIframeAPIReady() {
-      player = new YT.Player('player', {
-        height: '180',
-        width: '320',
-        videoId: 'BLF-f4kUtAw', // replace with your video id
-        events: {
-          'onReady': onPlayerReady,
-        }
-      });
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '180',
+    width: '320',
+    videoId: 'BLF-f4kUtAw', // replace with your video id
+    events: {
+      'onReady': onPlayerReady,
     }
+  });
+}
 
-    function onPlayerReady(event) {
-      event.target.playVideo(); // Play the video when it's ready
-    }
+function onPlayerReady(event) {
+  event.target.playVideo(); // Play the video when it's ready
+}
 
-    $(document).ready(function() {
-      // Start the marquee animation on page load
-      $(".marquee").animate({ left: "-100%" }, 60000, "linear", function() {
-        $(this).css("left", "100%");
-        if (!$(this).hasClass("paused")) {
-          startMarquee();
-        }
-      });
+$(document).ready(function() {
+  let animationInterval;
 
-      $(".marquee-container").hover(
-        function() {
-          $(".marquee").addClass("paused");
-          player.pauseVideo(); // Pause the video when the marquee is hovered over
-        },
-        function() {
-          $(".marquee").removeClass("paused");
-          player.playVideo(); // Play the video when the marquee is not being hovered over
-        }
-      );
+  function startMarquee() {
+    $(".marquee").animate({ left: "-100%" }, 60000, "linear", function() {
+      $(this).css("left", "100%");
+      if (!$(this).hasClass("paused")) {
+        startMarquee();
+      }
     });
+  }
+
+  // Start the marquee animation on page load
+  startMarquee();
+
+  $(".marquee-container").hover(
+    function() {
+      $(".marquee").addClass("paused");
+      player.pauseVideo(); // Pause the video when the marquee is hovered over
+    },
+    function() {
+      $(".marquee").removeClass("paused");
+      player.playVideo(); // Resume the video when the marquee is not being hovered over
+      if (!$(".marquee").hasClass("paused")) {
+        startMarquee(); // Resume the marquee animation if it was paused
+      }
+    }
+  );
+});
+  
 
 
   
