5d3788d5be2df3956c4d3f4da0721e7f6aab5148
kurziokode <108660505+kurziokode@users.noreply.github.com>
2023-10-06 15:03:02 +0300

    Update stream.js

--- morph/stream.js
+++ morph/stream.js
@@ -50,6 +50,11 @@ var tag = document.createElement('script');
   });
 */
 
+var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
 var player;
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
