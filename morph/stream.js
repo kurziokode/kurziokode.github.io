e36009dca2c2f6fbce66c41ae254cf59a0e59bc0
kurziokode <108660505+kurziokode@users.noreply.github.com>
2023-10-06 10:36:34 +0300

    Update stream.js

--- morph/stream.js
+++ morph/stream.js
@@ -5,9 +5,9 @@
 
   var player;
   function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player' 'audio-player-container' , {
-      height: '0',
-      width: '0',
+    player = new YT.Player('player' , {
+      height: '240',
+      width: '426',
       videoId: 'BLF-f4kUtAw', // replace with your video id
       events: {
         'onReady': onPlayerReady,
