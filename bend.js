c90bae0a627305bb550e429563e22b439361a783
kurziokode <108660505+kurziokode@users.noreply.github.com>
2024-04-22 21:47:51 +0300

    Create bend.js

---
+++ bend.js
@@ -0,0 +1,7 @@
+        $(".open-burger").on("click", function () {
+            $("#myNav").css("width", "100%");
+        });
+
+        $(".close-burger").on("click", function () {
+            $("#myNav").css("width", "0%");
+        });
