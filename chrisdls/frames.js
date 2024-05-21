d626849bcc35661e187f1e25aef79afc609c1901
kurziokode <lestiflee@protonmail.com>
2024-05-21 21:19:37 +0300

    Add files via upload

---
+++ chrisdls/frames.js
@@ -0,0 +1,9 @@
+var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        loop: true,
+        items: 1,
+        thumbs: true,
+        thumbImage: true,
+        thumbContainerClass: 'owl-thumbs',
+        thumbItemClass: 'owl-thumb-item'
+    });
